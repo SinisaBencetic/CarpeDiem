d3a735047337a1efbd1677234817ce0a4bbf59e3
Siniša <Benediktus>
2013-03-31 20:20:09 +0200

    GetTaskLastExecutionDate

---
+++ scripts/phonegapHelper.js
@@ -0,0 +1,12 @@
+﻿function GetTaskLastExecutionDate() {
+    //var db = window.openDatabase("test", "1.0", "Test DB", 1000000);
+    try {
+        var db = window.openDatabase("carpediem", "1.0", "Carpe diem", 1000);
+        if (db == undefined) {
+            alert("No DB object created...");
+            return undefined;
+        }
+    } catch (e) {
+        alert("Error while accessing DB..."+e.message);
+    }            
+}
