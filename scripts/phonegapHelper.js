776192bd4e7ff2003c126f563c273390bfceb52e
Siniša <Benediktus>
2013-03-31 20:34:45 +0200

    createDBifMissing

--- scripts/phonegapHelper.js
+++ scripts/phonegapHelper.js
@@ -1,12 +1,26 @@
-﻿function GetTaskLastExecutionDate() {
-    //var db = window.openDatabase("test", "1.0", "Test DB", 1000000);
+﻿function createDBifMissing(tx) {
+    tx.executeSql('DROP TABLE IF EXISTS task');
+    tx.executeSql('CREATE TABLE IF NOT EXISTS task (id unique, lastExecutionDate)');    
+}
+
+function errorCB(err) {
+    alert("Error processing SQL: " + err.code);
+}
+
+function successCB() {
+    alert("success!");
+}
+
+function GetTaskLastExecutionDate() {
     try {
         var db = window.openDatabase("carpediem", "1.0", "Carpe diem", 1000);
         if (db == undefined) {
             alert("No DB object created...");
             return undefined;
         }
+        db.transaction(createDBifMissing, errorCB, successCB);
     } catch (e) {
-        alert("Error while accessing DB..."+e.message);
+        alert("Error during GetTaskLastExecutionDate() ..." + e.message);
     }            
 }
+
