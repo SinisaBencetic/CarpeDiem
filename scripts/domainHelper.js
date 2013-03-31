00e3ba76569bdebbd4fd585ce03b0a30374e4b12
Siniša <Benediktus>
2013-03-31 22:39:16 +0200

    UpdateTaskDate del + insert

--- scripts/domainHelper.js
+++ scripts/domainHelper.js
@@ -1,6 +1,14 @@
-﻿function GetTaskDateMilisec(taskDOMid) {
+﻿function UpdateTaskDate(id, date) {
+    ExecuteTaskTransaction(CreateDBifMissing);
+    taskId = id.toString();
+    taskDate = date.toString();
+    ExecuteTaskTransaction(UpdateTaskDateSQL);
+}
+
+function GetTaskDateMilisec(taskDOMid) {
     var taskDate = $('#' + taskDOMid).val();
     if (taskDate == undefined || taskDate == 0) taskDate = new Date();    
     var taskMilisec = taskDate.getTime();
     alert(taskMilisec);
 }
+
--- scripts/phonegapHelper.js
+++ scripts/phonegapHelper.js
@@ -1,5 +1,7 @@
-﻿function createDBifMissing(tx) {
-    tx.executeSql('DROP TABLE IF EXISTS task');
+﻿var taskId = "";
+var taskDate = "";
+function CreateDBifMissing(tx) {
+    //tx.executeSql('DROP TABLE IF EXISTS task');
     tx.executeSql('CREATE TABLE IF NOT EXISTS task (id unique, lastExecutionDate)');
     //var id = 1;
     //var date = new Date().getTime();
@@ -18,27 +20,24 @@ function successCB() {
     alert("success!");
 }
 
-function updateTaskDate(tx) {
-    var id = 1;
-    var date = new Date().getTime();
-    //if (transaction == undefined) throw ('empty transaction!');
-    //if (id == undefined) throw ('task id empty!');
-    
-    tx.executeSql('INSERT INTO task (id, lastExecutionDate) VALUES('+id.toString()+',"'+date.toString()+'")');
-    //tx.executeSql('INSERT INTO task (id, lastExecutionDate) VALUES(1,"dummy")');
+function UpdateTaskDateSQL(tx) {
+    try {
+        alert(taskId + '|'+ taskDate);
+        tx.executeSql('DELETE FROM task WHERE id="'+taskId.toString()+'"');
+        tx.executeSql('INSERT INTO task (id, lastExecutionDate) VALUES("' + taskId.toString() + '","' + taskDate.toString() + '")');
+    } catch (e) {
+        alert('updateTaskDateSQL->' + e.message);
+    }
 }
 
-function GetTaskLastExecutionDate() {
+
+function ExecuteTaskTransaction(transactionSQL) {
     try {
         var db = window.openDatabase("carpediem", "1.0", "Carpe diem", 1000);
-        if (db == undefined) {
-            alert("No DB object created...");
-            return undefined;
-        }
-        db.transaction(createDBifMissing, errorCB, successCB);
-        db.transaction(updateTaskDate, errorCB, successCB);
+        if (db == undefined) throw("No DB object created...");                    
+        db.transaction(transactionSQL, errorCB, successCB);        
     } catch (e) {
-        alert("Error during GetTaskLastExecutionDate() ..." + e.message);
+        alert("Error during ExecuteSQL() ..." + e.message);
     }            
 }
 
